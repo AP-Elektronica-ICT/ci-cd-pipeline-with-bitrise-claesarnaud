c3ab38a2ad3928f00669884b35bd4fedab15b300
claesarnaud <arnaud.claes@hotmail.com>
2018-11-24 12:03:50 +0100

    first commit

---
+++ app/src/main/java/projectmanagement/claesarnaud/com/projectmanagement/Utils.java
@@ -0,0 +1,7 @@
+package projectmanagement.claesarnaud.com.projectmanagement;
+
+public class Utils {
+    public static int addNumbers(int first, int second){
+        return first + second;
+    }
+}
